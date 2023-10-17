fa33b0862a416727d61c633714fd81fa6a456238
Yaroslav <103814737+yaroslav1234561@users.noreply.github.com>
2023-10-17 22:31:32 +0700

    Update basket.js

--- js/basket.js
+++ js/basket.js
@@ -1,73 +1,80 @@
-let basketProductArray = [];
-checkLocalStorage();
-;
+import {
+  basketAdd,
+  checkLocalStorage,
+  quantityProduct,
+  addProductId,
+  quantityBasketProduct,
+  roundUpPrice,
+} from './module/cardFunction';
 
-function basketAdd(idElem) {
-  quantityProduct(idElem);
-}
+// let basketProductArray = [];
+checkLocalStorage;
+// function basketAdd(idElem) {
+//   quantityProduct(idElem);
+// }
 
-function checkLocalStorage() {
-  if (localStorage.basketProduct !== '') {
-    basketProductArray = JSON.parse(localStorage.basketProduct);
-    console.log(basketProductArray[0]);
-    
-  }
-  quantityBasketProduct(basketProductArray)
-}
+// function checkLocalStorage() {
+//   if (localStorage.basketProduct !== '') {
+//     basketProductArray = JSON.parse(localStorage.basketProduct);
+//     console.log(basketProductArray[0]);
+//   }
+//   quantityBasketProduct(basketProductArray);
+// }
 
-function quantityProduct(idElem) {
-  let ifPruductTrue = 0;
-  if (basketProductArray.length > 0) {
-    basketProductArray.forEach((element) => {
-      if (element.id === idElem) {
-        element.quantity = element.quantity + 1;
-        localStorage.basketProduct = JSON.stringify(basketProductArray);
-        ifPruductTrue = 1;
-        quantityBasketProduct(basketProductArray);
-      }
-    });
-    console.log(ifPruductTrue);
+// function quantityProduct(idElem) {
+//   let ifPruductTrue = 0;
+//   if (basketProductArray.length > 0) {
+//     basketProductArray.forEach((element) => {
+//       if (element.id === idElem) {
+//         element.quantity = element.quantity + 1;
+//         localStorage.basketProduct = JSON.stringify(basketProductArray);
+//         ifPruductTrue = 1;
+//         quantityBasketProduct(basketProductArray);
+//       }
+//     });
+//     console.log(ifPruductTrue);
 
-    if (ifPruductTrue === 0) {
-      addProductId(idElem);
-      ifPruductTrue = 0;
-    }
-  } else {
-    addProductId(idElem);
-  }
-}
-function addProductId(idElem) {
-  let arrayProduct = JSON.parse(localStorage.storage)
-  arrayProduct.forEach(element => {
-    if(idElem ===element.id)
-    basketProductArray.push({
-      id: element.id,
-      name: element.fields.name,
-      price: roundUpPrice(element.fields.price),
-      img: element.fields.image[0].url,
-      quantity: 1,
-    });
-  });
-  
-  localStorage.basketProduct = JSON.stringify(basketProductArray);
-  quantityBasketProduct(basketProductArray);
-}
+//     if (ifPruductTrue === 0) {
+//       addProductId(idElem);
+//       ifPruductTrue = 0;
+//     }
+//   } else {
+//     addProductId(idElem);
+//   }
+// }
 
-function quantityBasketProduct(basketProductArray) {
-  let sumProduct = 0;
-  basketProductArray.forEach((element) => {
-    sumProduct = sumProduct + element.quantity;
-  });
-  console.log(sumProduct);
-  document.getElementById('basket_summ_items').innerHTML = sumProduct;
-}
+// function addProductId(idElem) {
+//   let arrayProduct = JSON.parse(localStorage.storage);
+//   arrayProduct.forEach((element) => {
+//     if (idElem === element.id)
+//       basketProductArray.push({
+//         id: element.id,
+//         name: element.fields.name,
+//         price: roundUpPrice(element.fields.price),
+//         img: element.fields.image[0].url,
+//         quantity: 1,
+//       });
+//   });
+
+//   localStorage.basketProduct = JSON.stringify(basketProductArray);
+//   quantityBasketProduct(basketProductArray);
+// }
+
+// function quantityBasketProduct(basketProductArray) {
+//   let sumProduct = 0;
+//   basketProductArray.forEach((element) => {
+//     sumProduct = sumProduct + element.quantity;
+//   });
+//   console.log(sumProduct);
+//   document.getElementById('basket_summ_items').innerHTML = sumProduct;
+// }
 
 // localStorage.clear('basketProduct');
 
-function roundUpPrice(valueForRounding) {
-  let resultValue;
-  let maxValueStartTwoSymble = String(valueForRounding).slice(0, -2);
-  let maxValueEndTwoSymble = String(valueForRounding).slice(-2);
-  resultValue = Number(maxValueStartTwoSymble + '.' + maxValueEndTwoSymble);
-  return resultValue;
-}
+// function roundUpPrice(valueForRounding) {
+//   let resultValue;
+//   let maxValueStartTwoSymble = String(valueForRounding).slice(0, -2);
+//   let maxValueEndTwoSymble = String(valueForRounding).slice(-2);
+//   resultValue = Number(maxValueStartTwoSymble + '.' + maxValueEndTwoSymble);
+//   return resultValue;
+// }
