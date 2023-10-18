f2c9388b2a4d264b38e814a17d97e8a4690c4bdb
Yaroslav <103814737+yaroslav1234561@users.noreply.github.com>
2023-10-18 17:36:35 +0700

    Update cardFunction.js

--- js/module/cardFunction.js
+++ js/module/cardFunction.js
@@ -1,4 +1,4 @@
-import { basketProductArrays } from './storageValue';
+// import { basketProductArrays } from './storageValue.js';
 let basketProductArray = [];
 
 const curdFunction = {
@@ -6,11 +6,14 @@ const curdFunction = {
     curdFunction.quantityProduct(idElem);
   },
 
-  checkLocalStorage: function () {
+  checkLocalStorage1: function () {
     if (localStorage.basketProduct !== '') {
-      basketProductArray = basketProductArrays;
       console.log(basketProductArray[0]);
+      basketProductArray = JSON.parse(localStorage.basketProduct);
+      console.log(basketProductArray[0]);
+      console.log('dff');
     }
+    console.log('dff');
     curdFunction.quantityBasketProduct(basketProductArray);
   },
   quantityProduct: function (idElem) {
@@ -67,8 +70,120 @@ const curdFunction = {
 };
 
 export const basketAdd = curdFunction.basketAdd;
-export const checkLocalStorage = curdFunction.checkLocalStorage;
+export const checkLocalStorage = curdFunction.checkLocalStorage1;
 export const quantityProduct = curdFunction.quantityProduct;
 export const addProductId = curdFunction.addProductId;
 export const quantityBasketProduct = curdFunction.quantityBasketProduct;
 export const roundUpPrice = curdFunction.roundUpPrice;
+
+const $htmlBasketOpen = document.getElementById('basket_open');
+const $bodyBackground = document.getSelection('BackgroundForBasket');
+let htmlOptionsBasketProduct;
+let arrayProductsBaskets
+
+const curdOpen = {
+  basketOpen: function() {
+    arrayProductsBaskets = JSON.parse(localStorage.basketProduct);
+    let sumAllProductBasket = sumAllProductBaskets(arrayProductsBaskets);
+    let optionsBasketProduct = optionsBasketProducts(arrayProductsBaskets);
+    $htmlBasketOpen.classList.add('basket_open');
+    $htmlBasketOpen.classList.remove('basket_close');
+  
+    $htmlBasketOpen.innerHTML = `
+      <div class="block_basket">
+    <div class="close_basket" onclick="toDoBasketClose()"><i  class="bi bi-x"></i></div>
+    <div class="basket_block_main">
+      <h3 class="title_basket">Your Bag</h3>
+      <div class="block_basket_product">${optionsBasketProduct}</div>
+      <div class="div_prise_basket">
+        <p class="_prise_basket">Total : $${sumAllProductBasket}</p>
+      </div>
+      <button class="basket_checkout">checkout</button>
+  
+    </div>
+  
+  </div>`;
+    console.log('check');
+  },
+
+  sumAllProductBaskets: function (permArrayProductsBasket) {
+    let resultSumAllProductBasket = 0;
+    permArrayProductsBasket.forEach((element) => {
+      resultSumAllProductBasket =
+        resultSumAllProductBasket + element.quantity * element.price;
+    });
+    return resultSumAllProductBasket;
+  },
+
+  optionsBasketProducts: function (permArrayProductsBaskets) {
+    let htmlOptionsBasketProduct = curdOpen.sortThroughBasketProduct(permArrayProductsBaskets);
+    return htmlOptionsBasketProduct;
+  },
+
+  sortThroughBasketProduct: function (permArrayProductsBaskets) {
+    let htmlOptionsBasketProducts = '';
+    let indexObjProduct = 0
+    permArrayProductsBaskets.forEach((element) => {
+      console.log(indexObjProduct)
+      htmlOptionsBasketProducts =
+        htmlOptionsBasketProducts +
+        `
+        <div class="display_flex_basket_product">
+  <div class="img_elem_basket_product">
+    <img src="${element.img}" alt="">
+  </div>
+  <div class="text_elem_basket_product">
+    <p class="title_elem_basket_product">${element.name}</p>
+    <p class="price_elem_basket_product">${element.price}</p>
+    <button class="button_elem_basket_product" onclick="removeItemBasket(${indexObjProduct})">remove</button>
+  
+  </div>
+  <div class="quantity_elem_basket_product">
+    <button class="quantity_plus" onclick=productPlus(${indexObjProduct})><i class="bi bi-caret-up"></i></button>
+    <p class="quantity_text_basket_product">${element.quantity}</p>
+    <button class="quantity_minus" onclick=productMinus(${indexObjProduct})><i class="bi bi-caret-down"></i></button>
+  </div>
+  </div>`;
+  indexObjProduct++
+    });
+    return htmlOptionsBasketProducts;
+  },
+
+  basketClose: function() {
+    $htmlBasketOpen.innerHTML = ``;
+    $htmlBasketOpen.classList.remove('basket_open');
+    $htmlBasketOpen.classList.add('basket_close');
+  },
+
+  removeItemBasket: function (indexArr){
+    arrayProductsBaskets.splice(indexArr, 1);
+    localStorage.basketProduct = JSON.stringify(arrayProductsBaskets)
+    curdOpen.basketOpen()
+},
+
+productPlus: function(indexArr){
+  arrayProductsBaskets[indexArr].quantity++
+  localStorage.basketProduct = JSON.stringify(arrayProductsBaskets)
+  curdOpen.basketOpen()
+},
+
+productMinus: function (indexArr){
+  arrayProductsBaskets[indexArr].quantity--
+  if(arrayProductsBaskets[indexArr].quantity ===0){
+    curdOpen.removeItemBasket(indexArr)
+  } else{
+      localStorage.basketProduct = JSON.stringify(arrayProductsBaskets)
+      curdOpen.basketOpen()
+  }
+  
+},
+}
+
+export const basketOpen = curdFunction.basketOpen;
+export const sumAllProductBaskets = curdFunction.sumAllProductBaskets;
+export const optionsBasketProducts = curdFunction.optionsBasketProducts;
+export const sortThroughBasketProduct = curdFunction.sortThroughBasketProduct;
+export const basketClose = curdFunction.basketClose;
+export const removeItemBasket = curdFunction.removeItemBasket;
+export const productPlus = curdFunction.productPlus;
+export const productMinus = curdFunction.productMinus;
