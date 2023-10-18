d21630114c24082fc497b97bf96ec2ed623128e9
Yaroslav <103814737+yaroslav1234561@users.noreply.github.com>
2023-10-18 17:36:37 +0700

    Update storageValue.js

--- js/module/storageValue.js
+++ js/module/storageValue.js
@@ -1,4 +1,15 @@
-const storageValue = {
-  basketProductArray: JSON.parse(localStorage.basketProduct),
-};
-export const basketProductArrays = storageValue.basketProductArray;
+import {checkLocalStorage, basketOpen, basketAdd, basketOpen } from './module/cardFunction.js';
+checkLocalStorage;
+
+function toDoBasketClose(){
+  console.log("ghjikj")
+  basketClose(); 
+  checkLocalStorage();
+}
+function toDoBasketOpen(productsFeatured){
+  console.log("ghjikj")
+  console.log(productsFeatured)
+  basketAdd(productsFeatured);
+  basketOpen(); 
+  
+}
