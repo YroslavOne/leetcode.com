eb4d119067742bb447ae8ab91ccc13bd98b1aad9
Yaroslav <103814737+yaroslav1234561@users.noreply.github.com>
2023-10-18 17:36:33 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -32,7 +32,7 @@ function elementAssembly(productsFeatured) {
 <div class="home_background_featured" style="Background: url(${productsFeatured.fields.image[0].url}); background-size: cover; object-fit: contain; background-position: center">
 <div class="basket_and_loupe">
 <a href="./productsLoupe.html"><button onclick=loupeOnClick("${productsFeatured.id}") class="loupe"><i class="bi bi-search"></i></button></a>
-<button onclick=toDoBasketOpen("${productsFeatured.id}") class="basket_tap"><i class="bi bi-cart3"></i></button>
+<button onclick=toDoBasketOpen(${productsFeatured.id}) class="basket_tap"><i class="bi bi-cart3"></i></button>
 </div>
 </div>
 <h4 class="product_featured_name">${productsFeatured.fields.name}</h4>
